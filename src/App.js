e66f4ac8b8a1c434bea84c7b700d42215e81574d
rahmatagungj <rahmatagungj@gmail.com>
2021-05-29 07:54:26 +0700

    first init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Content from "./Components/Content/Index";
+import Navigation from "./Components/Navigation/Index";
+
+function App() {
+  return (
+    <>
+      <Content />
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navigation/Index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Project</li>
+        <li>Profile</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
