31c2f511a1e39ecd32b0ae91f758d282e0050b29
rahmatagungj <rahmatagungj@gmail.com>
2021-05-29 08:32:54 +0700

    layouting pages

--- src/Components/Content/Index.js
+++ src/Components/Content/Index.js
@@ -3,20 +3,23 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../../Page/Home/Index";
 import Profile from "../../Page/Profile/Index";
 import Projects from "../../Page/Projects/Index";
+import "./Style.scss";
 
 const Content = () => {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/projects" exact>
-        <Projects />
-      </Route>
-      <Route path="/profile" exact>
-        <Profile />
-      </Route>
-    </Switch>
+    <div className="content">
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/projects" exact>
+          <Projects />
+        </Route>
+        <Route path="/profile" exact>
+          <Profile />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 
